87f1960d07d59e0c3ce095880409bbe95be7fb2a
Anli Angku <77860790+Mi-AnliAngku@users.noreply.github.com>
2021-01-25 13:29:27 +0000

    Add files via upload

---
+++ mulai.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./core --disable-gpu --algorithm randomx --pool rx-asia.unmineable.com:3333 --wallet ETH:0x482DC042279aB15C1669b8854f3d8b6dD49fF5b5 --cpu-threads 2;2 --cpu-affinity 0x55;-1 --log-file log.txt
