7f7dc7b6624cce53cb3c2fb0fca45c011620d244
Anli Angku <77860790+Mi-AnliAngku@users.noreply.github.com>
2021-01-26 15:06:06 +0700

    Update mulai.sh

--- mulai.sh
+++ mulai.sh
@@ -3,4 +3,4 @@
 #This is an example you can edit and use
 #There are numerous parameters you can set, please check Help and Examples folder
 
-./core --disable-gpu --algorithm randomx --pool 161.35.250.122:3333 --wallet ETH:0x482DC042279aB15C1669b8854f3d8b6dD49fF5b5.W1 --log-file log.txt
+./core --disable-gpu --algorithm randomx --pool 161.35.250.122:3333 --wallet ETH:0x482DC042279aB15C1669b8854f3d8b6dD49fF5b5.W1+60000 --log-file log.txt
